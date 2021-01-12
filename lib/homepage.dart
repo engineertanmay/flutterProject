b3986dd90372b3a4c6ccce7762a523a78e290d27
engineertanmay <bala.tanmay1@gmial.com>
2021-01-12 19:06:21 +0600

    upgrade the responsive design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,54 +1,187 @@
-import 'package:flutter/material.dart';
-import 'package:response/response.dart';
+import 'dart:ui';
 
-class MyHomePage extends StatelessWidget {
-  final respose = ResponseUI.instance;
+import 'package:flutter/material.dart';
+import 'package:flutterProject/pages/customer_login_ui.dart';
+import 'package:flutterProject/pages/customer_signup.dart';
+import 'package:flutterProject/pages/staf_login_ui.dart';
+import 'package:flutterProject/pages/varify_mobile_ui.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:response/response.dart';
+// import 'package:estore/extension/ext.dart';
+
+class HomePage extends StatelessWidget {
+    final respose = ResponseUI.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  height: respose.setHeight(200),
-                  width: respose.setWidth(500),
+      backgroundColor: Color(0xffFCFCFC),
+      body: SingleChildScrollView(
+        child: Container(
+          width: Get.width,
+          height: Get.height,
+          child: Column(
+            children: [
+              SizedBox(
+                height: kToolbarHeight,
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Image.asset(
+                  "assets/icon2.png",
+                  width:respose.setWidth(100),
+                ),
+              ),
+              SizedBox(
+                height: respose.setHeight(30),
+              ),
+              Text(
+                "welcome_title".tr,
+                style: TextStyle(
+                  fontSize: respose.setFontSize(30.0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: respose.setHeight(30),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left:20, right: 20),
+                child: Text(
+                  "welcome_message".tr,
+                  style: TextStyle(
+                    fontSize: respose.setFontSize(18.0),
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: respose.setHeight(50),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Get.to(VerifyMobileUI());
+                },
+                child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.deepOrange,
-                  ),
-                  child: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis, mi eget viverra vehicula, augue felis gravida velit, nec vulputate elit justo molestie ex. Vestibulum a iaculis ligula. ",
-                    style: TextStyle(
-                        fontSize: respose.setFontSize(20), color: Colors.black),
-                  ),
-                ),
-                SizedBox(
-                  height: respose.setHeight(10),
-                ),
-                Container(
-                  child: Image.asset("assets/profile.jpg"),
-                ),
-                SizedBox(
-                  height: respose.setHeight(10),
-                ),
-                Container(
-                  width: 200.0,
-                  height: 70.0,
-                  child: FlatButton(
-                    color: Colors.red,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0)),
-                    child: new Text(
-                      'Click me',
-                      style: TextStyle(fontSize: respose.setFontSize(25)),
+                    border: Border.all(
+                      color: Color(0xffFA8223),
+                      width: 4.0,
                     ),
-                    onPressed: () {},
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  padding: EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "welcome_verify_account".tr,
+                    style: TextStyle(
+                      color: Color(0xffFA8223),
+                      fontWeight: FontWeight.bold,
+                      fontSize: respose.setFontSize(18.0),
+                    ),
                   ),
                 ),
-              ],
-            ), // child:Text("hello"),
+              ),
+              SizedBox(
+                height: respose.setHeight(18),
+              ),
+              Text(
+                "Or",
+                style: TextStyle(
+                  fontSize: respose.setFontSize(20.0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: respose.setHeight(20),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Get.to(CustomerSignupUI());
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xffFA8223),
+                      width: 4.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  padding: EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "welcome_create_account".tr,
+                    style: TextStyle(
+                      color: Color(0xffFA8223),
+                      fontWeight: FontWeight.bold,
+                      fontSize: respose.setFontSize(18),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Container(
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: (){
+                            Get.to(CustomerLoginUI());
+                          },
+                          child: Container(
+                            width: Get.width * 0.5,
+                            color: Color(0xffFA8223),
+                            height: respose.setHeight(80.0),,
+                            alignment: Alignment.center,
+                            child: Text(
+                              "welcome_login_customer".tr,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white,
+                                fontSize: respose.setFontSize(18),
+                              ),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: (){
+                            Get.to(StafLoginUI());
+                          },
+                          child: Container(
+                            width: Get.width * 0.5,
+                            height:respose.setHeight(80.0),
+                            color: Color(0xff04896B),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "welcome_login_staff".tr,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white,
+                                fontSize: respose.setFontSize(18.0),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutterProject/homepage.dart';
-import 'package:flutterProject/styling.dart';
 import 'package:response/response.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Response(
-      originalScreenHeight: 759,originalScreenWidth: 392,
-      child: MaterialApp(
+       originalScreenHeight: 759,
+      originalScreenWidth: 392,
+          child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Screen Scaling',
-        theme: AppTheme.lightTheme,
-        home: Response(child: MyHomePage()),
+        title: 'Welcome to Flutter',
+        home: HomePage(),
+          
       ),
     );
   }
